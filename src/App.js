84f611d712fb217af8b1d0c9a12337c29d97973c
egor <bv95.egor@gmail.com>
2022-08-25 23:54:10 +1200

    Create new repos

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React from 'react';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
+import Profile from './Components/Profile';
+
+const App = () => {
+  return (
+    <div className='app-wrapper'>
+      
+      <Header />
+      <NavBar />  
+      <Profile />
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        
+        <div className='content'>
+        Main content
+        <div>
+          <img src='https://bipbap.ru/wp-content/uploads/2017/04/0_7c779_5df17311_orig.jpg'></img>
+        </div>
+          <div>
+            avatar + description
+          </div>
+          <div> 
+            My post
+            <div>
+              New post
+            </div>
+            <div>
+              post1
+            </div>
+            <div>
+              post2
+            </div>
+          </div>
+      </div>
+    
+    )
+};
+
+export default Profile;
